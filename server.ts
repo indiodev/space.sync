6b5c97e8b4208262d45155b4d0bd6c3587c98687
jhollyfer <jhollyfer.fr@gmail.com>
2024-04-19 19:28:41 -0500

    chore: remove code

--- server.ts
+++ server.ts
@@ -21,42 +21,7 @@ APP.register(Socket, {
 });
 
 type VenomCreateOptions = CreateOptions & { multidevice?: boolean };
-
-// async function sendMessage(message: Message) {
-// 	console.log(message);
-// }
-
-// async function Start(client: Whatsapp) {
-// 	try {
-// 		// await client.onMessage(sendMessage);
-// 		const groups = await client.getAllChatsGroups();
-// 		console.log({ groups_total: groups.length, groups });
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// }
-
 APP.register(TriggerRoute, { prefix: '/trigger' });
-
-APP.post('/send-message', async (request, response) => {
-	if (!whatsapp || !whatsapp.isConnected()) {
-		return response.status(404).send({ message: 'Whatsapp not connected' });
-	}
-	await whatsapp.sendText(
-		'120363272121267113@g.us',
-		`_Ofertas exclusivas de sorvete para você!_\n*Supervisa Atacadista! Melhor preço todo dia!*
-		`,
-	);
-
-	await whatsapp.sendImage(
-		'120363272121267113@g.us',
-		// './public/image.jpeg',
-		'https://imgs.search.brave.com/GVhv2H-5YUyfUhNneCOZ-FMECzXCg4d7tNusD-Il5ek/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudmV4ZWxzLmNv/bS9tZWRpYS91c2Vy/cy8zLzMyMjMxMS9p/c29sYXRlZC9wcmV2/aWV3LzU2M2E1ZjUz/ZTA4ODgzMjIzODYw/OGI0ZTc1MDljNjY4/LXBpcmVzLWRlLXNv/cnZldGUucG5n',
-		'image.jpeg',
-		'',
-	);
-	return response.status(200).send({ message: 'Message sent' });
-});
 
 APP.get('/groups', async (request, response) => {
 	if (!whatsapp || !whatsapp.isConnected()) {
