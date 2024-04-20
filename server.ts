3958929ab57c823d946faa299ba513770bffe240
jhollyfer <jhollyfer.fr@gmail.com>
2024-04-19 19:27:28 -0500

    configure socketio

--- server.ts
+++ server.ts
@@ -1,15 +1,23 @@
 import Static from '@fastify/static';
 import { TriggerRoute } from 'app/routes/trigger';
 import { Env } from 'config/env';
-import { createServer } from 'http';
+import Socket from 'fastify-socket.io';
 import { join } from 'path';
 import { Server } from 'socket.io';
-import { create as Create, CreateOptions, Whatsapp } from 'venom-bot';
+import { CreateOptions, create as CreateVenom, Whatsapp } from 'venom-bot';
 import { APP } from './app';
+
 let whatsapp: Whatsapp | null = null;
+
 APP.register(Static, {
 	root: join(__dirname, 'public'),
 	prefix: '/public/',
+});
+
+APP.register(Socket, {
+	cors: {
+		origin: '*',
+	},
 });
 
 type VenomCreateOptions = CreateOptions & { multidevice?: boolean };
@@ -27,21 +35,6 @@ type VenomCreateOptions = CreateOptions & { multidevice?: boolean };
 // 		console.error(error);
 // 	}
 // }
-
-const SERVER = createServer(APP.server);
-
-const IO = new Server(SERVER, {
-	cors: {
-		origin: '*',
-	},
-});
-
-IO.on('connection', (socket) => {
-	console.log('a user connected');
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
-});
 
 APP.register(TriggerRoute, { prefix: '/trigger' });
 
@@ -81,15 +74,41 @@ APP.get('/groups', async (request, response) => {
 	return response.status(200).send(groups);
 });
 
+APP.get('/', async (request, response) => {
+	return response.sendFile('index.html');
+});
+
+APP.ready((err) => {
+	if (err) throw err;
+
+	APP.io.on('connection', (socket) => {
+		console.info('Socket connected!', socket.id);
+
+		socket.emit('message', 'Hello World!');
+
+		socket.on('generate qrcode', async () => {
+			whatsapp = await CreateVenom({
+				session: 'space_sync',
+				logQR: true,
+				multidevice: false,
+			} as VenomCreateOptions);
+		});
+
+		socket.on('disconnect', () => {
+			console.info('Socket disconnected!', socket.id);
+		});
+	});
+});
+
 APP.listen({
 	host: '0.0.0.0',
 	port: Env.APP_PORT,
 }).then(async () => {
 	console.log(`Server is running on http://localhost:${Env.APP_PORT}`);
-
-	whatsapp = await Create({
-		session: 'space_sync',
-		logQR: true,
-		multidevice: false,
-	} as VenomCreateOptions);
 });
+
+declare module 'fastify' {
+	export interface FastifyInstance {
+		io: Server;
+	}
+}
